d4e31f8affa3a28ff54ac1bd5bf9356446b1bab2
Rafael Silveira <114193420+Rafaelko2@users.noreply.github.com>
2023-03-05 11:41:02 -0300

    Subir

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Estudante de BCET", "Estudante de TI", "Apaixonado por programação"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
